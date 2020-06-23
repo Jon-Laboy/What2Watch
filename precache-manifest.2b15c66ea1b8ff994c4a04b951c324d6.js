557a4c1b9203f4c00aba8269a852e9b1898104ea
Jon Laboy <jonlaboyoc@gmail.com>
2020-06-22 20:25:23 -0400

    Updates

---
+++ precache-manifest.2b15c66ea1b8ff994c4a04b951c324d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b7e678581660d275740d10151c2377f",
+    "url": "/What2Watch/index.html"
+  },
+  {
+    "revision": "f6bc6009d3aadc40c2ce",
+    "url": "/What2Watch/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a04ab9bdd5405d72e498",
+    "url": "/What2Watch/static/css/main.437eafda.chunk.css"
+  },
+  {
+    "revision": "f6bc6009d3aadc40c2ce",
+    "url": "/What2Watch/static/js/2.7e96e135.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/What2Watch/static/js/2.7e96e135.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a04ab9bdd5405d72e498",
+    "url": "/What2Watch/static/js/main.c4b1b6c4.chunk.js"
+  },
+  {
+    "revision": "371622f10eba1155cee9",
+    "url": "/What2Watch/static/js/runtime-main.06d5d5ad.js"
+  },
+  {
+    "revision": "31d26327470d3d7f96f7bed3417fe659",
+    "url": "/What2Watch/static/media/logo.31d26327.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/What2Watch/precache-manifest.2b15c66ea1b8ff994c4a04b951c324d6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/What2Watch/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
