01f0a3580ed2b1a95e17bf88bf60a437a6f16736
Jon Laboy <jonlaboyoc@gmail.com>
2025-04-01 13:48:17 -0400

    chore: package json add home for gh page deploy

--- src/utils/filterShowsMovies.ts
+++ src/utils/filterShowsMovies.ts
@@ -1,3 +1,5 @@
-export const onlyResultsWithPosters = (data) => {
+import { MovieData } from "../types/types"
+
+export const onlyResultsWithPosters = (data: MovieData[]) => {
     return data?.filter(item => item.poster_path != null)
 }
